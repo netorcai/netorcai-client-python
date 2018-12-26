492dc4c5b7e8e16dea17eb9e54856adb6ce9cc93
Millian Poquet <millian.poquet@inria.fr>
2018-12-26 22:03:19 +0100

    [misc] packaging basics (setup.py)

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(name='netorcai',
+      version='0.1.0',
+      description='Python version of the netorcai client library',
+      author='Millian Poquet',
+      author_email='millian.poquet@gmail.com',
+      url='https://github.com/netorcai/netorcai-client-python/',
+      packages=['netorcai'],
+ )
