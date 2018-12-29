e1312eadd9c642ea6071cc4dee5dce6500b5fd65
Millian Poquet <millian.poquet@inria.fr>
2018-12-29 17:18:53 +0100

    [test] finish Client tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import pytest
 import subprocess
 import shlex
 from netorcai.client import *
@@ -47,4 +47,124 @@ def test_everything_goes_well():
     gl.send_do_turn_ack({"all_clients": {"gl": "python"}}, -1)
 
     player.read_game_ends()
+    n.terminate()
+
+def get_kicked_client():
+    c = Client()
+    c.connect()
+    c.send_string("¿qué?")
+    return c
+
+def test_kicked_instead_of_expected_message():
+    n = launch_netorcai_wait_listening(10, 20)
+    kick_reason = 'Kicked from netorcai. Reason: Invalid first message: Non-JSON message received'
+
+    with pytest.raises(RuntimeError) as err:
+        c = get_kicked_client()
+        c.read_login_ack()
+    print(kick_reason)
+    print(str(err))
+    assert kick_reason in str(err)
+
+    with pytest.raises(RuntimeError) as err:
+        c = get_kicked_client()
+        c.read_game_starts()
+    assert kick_reason in str(err)
+
+    with pytest.raises(RuntimeError) as err:
+        c = get_kicked_client()
+        c.read_turn()
+    assert kick_reason in str(err)
+
+    with pytest.raises(RuntimeError) as err:
+        c = get_kicked_client()
+        c.read_game_ends()
+    assert kick_reason in str(err)
+
+    with pytest.raises(RuntimeError) as err:
+        c = get_kicked_client()
+        c.read_do_init()
+    assert kick_reason in str(err)
+
+    with pytest.raises(RuntimeError) as err:
+        c = get_kicked_client()
+        c.read_do_turn()
+    assert kick_reason in str(err)
+
+    n.terminate()
+
+def get_logged_player():
+    c = Client()
+    c.connect()
+    c.send_login("I", "player")
+    return c;
+
+def test_unexpected_message_but_not_kick():
+    n = launch_netorcai_wait_listening(2, 0)
+    kick_reason = 'Unexpected message type received: LOGIN_ACK'
+
+    # LOGIN_ACK instead of something else
+    with pytest.raises(RuntimeError) as err:
+        c = get_logged_player()
+        c.read_game_starts()
+    assert kick_reason in str(err)
+
+    with pytest.raises(RuntimeError) as err:
+        c = get_logged_player()
+        c.read_turn()
+    assert kick_reason in str(err)
+
+    with pytest.raises(RuntimeError) as err:
+        c = get_logged_player()
+        c.read_game_ends()
+    assert kick_reason in str(err)
+
+    with pytest.raises(RuntimeError) as err:
+        c = get_logged_player()
+        c.read_do_init()
+    assert kick_reason in str(err)
+
+    with pytest.raises(RuntimeError) as err:
+        c = get_logged_player()
+        c.read_do_turn()
+    assert kick_reason in str(err)
+    c.close()
+
+    # Start a game
+    player1 = get_logged_player()
+    player2 = get_logged_player()
+
+    gl = Client()
+    gl.connect()
+    gl.send_login("gl", "game logic")
+
+    player1.read_login_ack()
+    player2.read_login_ack()
+    gl.read_login_ack()
+
+    # The game should start automatically (--autostart)
+    doInit = gl.read_do_init()
+    gl.send_do_init_ack({"all_clients": {"gl": "python"}})
+
+    # Player1 fails now
+    with pytest.raises(RuntimeError) as err:
+        player1.read_login_ack()
+    assert 'Unexpected message type received: GAME_STARTS' in str(err)
+    player2.read_game_starts()
+
+    for i in range(doInit.nb_turns_max - 1):
+        gl.read_do_turn()
+        gl.send_do_turn_ack({"all_clients": {"gl": "python"}}, -1)
+
+        turn = player2.read_turn()
+        player2.send_turn_ack(turn.turn_number, [{"player": "python"}])
+
+    gl.read_do_turn()
+    gl.send_do_turn_ack({"all_clients": {"gl": "python"}}, -1)
+
+    # Player2 fails now
+    with pytest.raises(RuntimeError) as err:
+        player2.read_turn()
+    assert 'Game over!' in str(err)
+
     n.terminate()
