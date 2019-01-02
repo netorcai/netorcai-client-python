76ab1069ec8b5a9e5d0e2d9dc341fc182eec6bb2
Millian Poquet <millian.poquet@inria.fr>
2019-01-02 23:33:08 +0100

    [misc] setup.py: remove long description

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setup(name='netorcai',
       version='1.0.0',
       description='Python version of the netorcai client library',
-      long_description=long_description,
       author='Millian Poquet',
       author_email='millian.poquet@gmail.com',
       url='https://github.com/netorcai/netorcai-client-python/',
