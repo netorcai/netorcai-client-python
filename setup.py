2c233e729427d2e0dc33b7afcf462970b9116e2d
Millian Poquet <millian.poquet@inria.fr>
2019-01-02 23:40:23 +0100

    [misc] bump version 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='netorcai',
-      version='1.0.0',
+      version='1.0.1',
       description='Python version of the netorcai client library',
       author='Millian Poquet',
       author_email='millian.poquet@gmail.com',
