eeeeacfd3ab6440a383d437399a90bc463c85a97
Roxane <roxane.bellot@gmail.com>
2019-09-26 15:53:49 +0200

    add __repr__ for all message types

--- netorcai/message.py
+++ netorcai/message.py
@@ -10,12 +10,18 @@ class PlayerInfo:
         self.remote_address = o["remote_address"]
         self.is_connected = o["is_connected"]
 
+    def __repr__(self):
+        return "<PlayerInfo:%s id=%s>" % (self.nickname, self.player_id)
+
 
 class PlayerActions:
     def __init__(self, o):
         self.player_id = o["player_id"]
         self.turn_number = o["turn_number"]
         self.actions = o["actions"]
+
+    def __repr__(self):
+        return "<PlayerActions:%s>" %  self.__dict__
 
 
 class LoginAckMessage:
@@ -24,6 +30,9 @@ class LoginAckMessage:
         if self.metaprotocol_version != metaprotocol_version():
             print("Warning: netorcai uses version '{}' while netorcai-client-python uses '{}'".format(
                 self.metaprotocol_version, metaprotocol_version()))
+
+    def __repr__(self):
+        return "<LoginAckMessage:%s>" % self.__dict__
 
 
 class GameStartsMessage:
@@ -37,11 +46,18 @@ class GameStartsMessage:
         self.players_info = [PlayerInfo(info) for info in o["players_info"]]
         self.initial_game_state = o["initial_game_state"]
 
+    def __repr__(self):
+        return "<GameStartsMessage:nb_players=%s nb_special_players=%s nb_turns_max=%s>" % (
+            self.nb_players, self.nb_special_players, self.nb_turns_max)
+
 
 class GameEndsMessage:
     def __init__(self, o):
         self.winner_player_id = o["winner_player_id"]
         self.game_state = o["game_state"]
+
+    def __repr__(self):
+        return "<GameEndsMessage:winner=%s>" % self.winner_player_id
 
 
 class TurnMessage:
@@ -50,6 +66,9 @@ class TurnMessage:
         self.players_info = [PlayerInfo(info) for info in o["players_info"]]
         self.game_state = o["game_state"]
 
+    def __repr__(self):
+        return "<TurnMessage for turn %s>" % self.turn_number
+
 
 class DoInitMessage:
     def __init__(self, o):
@@ -57,7 +76,14 @@ class DoInitMessage:
         self.nb_special_players = o["nb_special_players"]
         self.nb_turns_max = o["nb_turns_max"]
 
+    def __repr__(self):
+        return "<DoInitMessage:nb_players=%s nb_special_players=%s nb_turns_max=%s>" % (
+            self.nb_players, self.nb_special_players, self.nb_turns_max)
+
 
 class DoTurnMessage:
     def __init__(self, o):
         self.player_actions = [PlayerActions(action) for action in o["player_actions"]]
+
+    def __repr__(self):
+        return "<DoTurnMessage:%s>" % self.__dict__
